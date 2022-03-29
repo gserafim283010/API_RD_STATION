af619ec57801c5d7a8ad5fdccebb013d028dbf07
Gserafim <guisilvaserafim@gmail.com>
2022-03-29 09:20:13 -0300

    alteração

--- api.py
+++ api.py
@@ -1,64 +1,79 @@
-
-
 import requests
 import json
 
-# esse link pode mudar se os donos da API mudarem
-#cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
-#cotacoes_dic = cotacoes.json()
-#cotacao_dolar = cotacoes_dic['USDBRL']['bid']
-lista_campos = requests.get("https://plugcrm.net/api/v1/tasks?token=6205315036fb10000fb934fa")
+lista_campos = requests.get("https://plugcrm.net/api/v1/deals?token=6205315036fb10000fb934fa")
 rd_teste_j2 = lista_campos.json()
 
-
 for key in rd_teste_j2:
-    if key == "tasks":
+    if key == "deals":
         for key2 in rd_teste_j2[key]:
-            deal_id = ""
-            subject = ""
-            type = ""
-            hour = ""
-            data = ""
-            user_ids = ""
+            page = ""
+            name = ""
+            win = ""
+            user_id = ""
+            closed_at = ""
+            closed_at_period = ""
+            created_at_period = ""
+            prediction_date_period = ""
+            start_date = ""
+            end_date = ""           
+            campaign_id = ""
+            deal_stage_id = ""
+            deal_lost_reason_id = ""
+            hold = ""
+            
             
             for key3 in key2:
-                if key3 == "deal_id":
-                    deal_id = key2[key3]
-                if key3 == "subject":
-                    subject = key2[key3]
-                if key3 == "type":
-                    type = key2[key3]    
-                if key3 == "hour":
-                    hour = key2[key3]
-                if key3 == "data":
-                    data = key2[key3] 
-                if  key3 == "user_ids": 
-                    user_ids = key2[key3]
-print(rd_teste_j2)    
-                                   
-            # params = {
-            #     'token':'6205315036fb10000fb934fa',
-            #     'deal_id': deal_id,
-            #     'subject' : subject,
-            #     'type' : type,
-            #     'hour' : hour,
-            #     'data' : data,
-            #     'user_ids' : user_ids                
-            # }
+                if key3 == "page":
+                    page = key2[key3]
+                if key3 == "name":
+                    name = key2[key3]
+                if key3 == "win":
+                    win = key2[key3]    
+                if key3 == "user_id":
+                    user_id = key2[key3]
+                if key3 == "closed_at":
+                    closed_at = key2[key3] 
+                if  key3 == "closed_at_period": 
+                    closed_at_period = key2[key3]
+                if key3 =='created_at_period': 
+                   created_at_period = key2[key3] 
+                if key3 == 'prediction_date_period':
+                   prediction_date_period = key2[key3]
+                if key3 == 'start_date':
+                   start_date = key2[key3]
+                if key3 == 'end_date':
+                   end_date = key2[key3]
+                if key3 == 'campaign_id':         
+                   campaign_id = key2[key3]
+                if key3 == 'deal_stage_id':
+                   deal_stage_id = key2[key3]  
+                if key3 == 'deal_lost_reason_id':
+                    deal_lost_reason_id = key2[key3]
+                if key3 == 'hold':
+                  hold = key2[key3]  
+                  
+    
+    
+    # print(rd_teste_j2)    
+        params = {'page':page,
+                  'name':name,
+                  'win':win,
+                  'user_id':user_id,
+                  'closed_at':closed_at,
+                  'created_at_period':created_at_period,
+                  'prediction_date_period':prediction_date_period,
+                  'start_date':start_date,
+                  'end_date' :end_date, 
+                  'campaign_id':campaign_id,
+                  'deal_lost_reason_id':deal_lost_reason_id,
+                  'deal_stage_id':deal_stage_id,
+                  'hold':hold
+                  }
 
-            # rd_teste = requests.post("https://plugcrm.net/api/v1/tasks",params)
-            # rd_teste_j =rd_teste.json()
-            # print(rd_teste_j)
+rd_teste = requests.get("https://plugcrm.net/api/v1/deals?token=6205315036fb10000fb934fa",'params')
+rd_teste_j = rd_teste.json()
+print(rd_teste_j)
+#.
            
-       #
-         #   funcao_createTask({'deal_id': deal_id})     
-                    
-
-
-#funcao_createTask(obj){
-
-
-#rd_api = rd_teste['page']['limit']['deal_id']['done']
-
-#print(rd_teste_j)  
-#}
+       
